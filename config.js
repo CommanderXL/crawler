f28d0cb49ad22008c9859e0514f0f3aa53936c84
XRene <XRene@foxmail.com>
2015-05-17 23:26:46 +0800

    配置文件

---
+++ config.js
@@ -0,0 +1,8 @@
+var config={
+	mongodbURL:"mongodb://localhost/crawler",
+	port:3000,
+	//豆瓣cookie
+	cookie:'bid="jfmxTpK9l7A"; ll="108288"; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1431824087%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DYfDeFQXxIzCdEi2BjqJc461p5Kbal68jBsDx9-c05Fs3tl5Okr2xUVhG-KLk_jGc%26wd%3D%26eqid%3Dd0e9026900003e3c000000025556b002%26ie%3Dutf-8%22%5D; __utmt=1; _pk_id.100001.8cb4=fc1070a32e79c04b.1428739160.13.1431824167.1431779489.; _pk_ses.100001.8cb4=*; __utma=30149280.1267262723.1428739160.1431779514.1431824088.12; __utmb=30149280.13.10.1431824088; __utmc=30149280; __utmz=30149280.1431751825.10.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; dbcl2="60846196:I4LsTMBiDhw',
+	gaid: 'UA-26476625-4'
+}
+module.exports=config;
